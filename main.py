38c853a240dac78bb401d183fd3559738c09d4e1
TrungKhue <119155698+TrungKhue@users.noreply.github.com>
2023-09-09 22:36:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 recognizer = sr.Recognizer()
 with sr.Microphone() as source:
-    print("Nói một câu:")
+    print("Say a word:")
     audio = recognizer.listen(source)
 try:
     spoken_text = recognizer.recognize_google(audio)
@@ -12,8 +12,8 @@ try:
     total_words = len(target_words)
     correct_words = sum(1 for word in spoken_words if word in target_words)
     accuracy = (correct_words / total_words) * 100
-    print("Tỷ lệ từ bạn nói đúng: ", accuracy, "%")
+    print("The percentage of words you say right: ", accuracy, "%")
 except sr.UnknownValueError:
-    print("Không thể nhận dạng giọng nói.")
+    print("Can't recognize voice.")
 except sr.RequestError as e:
-    print("Lỗi trong quá trình gửi yêu cầu đến Google: {0}".format(e))
+    print("Error submitting request to Google: {0}".format(e))
